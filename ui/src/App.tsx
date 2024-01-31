94a4a009f2cf1b1b5b4ac5b191edf806381041af
Shivendra Saurav <shivendrasaurav@gmail.com>
2024-01-31 16:58:48 +0530

    fixed build warnings

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
 import {Fragment} from "react";
-import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import 'fdweb/fluent-core.css';
 import 'fdweb/fluent-icons.css';
 import './App.css';
