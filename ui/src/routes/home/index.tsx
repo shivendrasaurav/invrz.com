5ee61b5f5ca5782b73a70b6e16f5b5973213422d
Shivendra Saurav <shivendrasaurav@gmail.com>
2024-03-08 13:06:58 +0530

    updated hero

--- ui/src/routes/home/index.tsx
+++ ui/src/routes/home/index.tsx
@@ -1,4 +1,4 @@
-import {Fragment} from "react";
+import {Fragment, useEffect, useState} from "react";
 import "./home.styles.css";
 import lunasyzImg from "../../assets/lunasyz.png";
 import ctnImg from "../../assets/ctn.png";
@@ -6,6 +6,36 @@ import fdwebImg from "../../assets/fdweb.png";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
+
+    const randomBgPalleteGenerator = () => {
+
+        const redMain = Math.round(Math.abs(Math.random() * (171 - 255) + 0));
+        const greenMain = Math.round(Math.abs(Math.random() * (86 - 170) + 0));
+        const blueMain = Math.round(Math.abs(Math.random() * (0 - 85) + 0));
+
+        const bgPrime = `rgb(${redMain*0.7}, ${greenMain*0.7}, ${blueMain*0.7})`;
+        const textPrime = `rgb(${255 - redMain}, ${255 - greenMain}, ${255 - blueMain})`
+        document.getElementById("hero").style.backgroundColor = bgPrime;
+        document.getElementById("hero").style.color = textPrime;
+
+        
+        const bgNav = `rgb(${redMain*0.4}, ${greenMain*0.4}, ${blueMain*0.4})`;
+        const textNav = `rgb(${255 - redMain*0.4}, ${255 - greenMain*0.4}, ${255 - blueMain*0.4})`
+        document.getElementById("sideNav").style.backgroundColor = bgNav;
+        document.getElementById("sideNav").style.color = textNav;
+        
+
+        const bgBody = `rgb(${redMain*1.8}, ${greenMain*1.8}, ${blueMain*1.8})`;
+        const textBody = `rgb(${255 - redMain*1.8}, ${255 - greenMain*1.8}, ${255 - blueMain*1.8})`
+        document.getElementsByTagName("body")[0].style.backgroundColor = bgBody;
+        document.getElementsByTagName("body")[0].style.color = textBody;
+
+        const pills = document.getElementsByClassName("navLink");
+        for(let i=0; i<pills.length; i++){
+            pills[i].style.backgroundColor = textNav;
+        }
+        
+    }
 
     const dateToday = new Date().toDateString();
 
@@ -16,10 +46,16 @@ const Home = () => {
         }
     }
 
+    useEffect(()=>{
+
+        randomBgPalleteGenerator();
+
+    }, [])
+
     return(
         <Fragment>
             <span id="top"></span>
-            <div className="sideNavWrapper">            
+            <div className="sideNavWrapper" id="sideNav">            
                 <a onClick={() => scrollToDiv("hero")} className="navLink hero">H</a>
                 <a onClick={() => scrollToDiv("about")} className="navLink about">A</a>
                 <a onClick={() => scrollToDiv("projects")} className="navLink projects">P</a>
@@ -34,47 +70,10 @@ const Home = () => {
                 </div>
             </div>
             <div className="aboutWrapper" id="about">
-                <div className="aboutCard">
-                    <p className="aboutTitle">About invrz.com</p>
-                    <p className="aboutText">
-                        We're invrz.com, a tech collective where code hums alongside the roar of the crowd. We craft sleek apps that bring your digital world to life, from sleek interfaces to intuitive experiences. Whether it's a game that ignites your passion or a platform that connects you to the pulse of the latest sports headlines, we pour our energy into weaving technology with your aspirations.
-                        <br/>
-                        But what truly defines us is the thrill of pushing boundaries. We revel in the challenge of reimagining possibilities, flipping expectations on their heads, and weaving technology into experiences that transcend the ordinary.
-                        <br/>
-                        So, whether you're envisioning a groundbreaking app, a website that dances with elegance, or a platform that rewrites the rules of the game, invrz.com is your portal to a reality where technology bends to your will. Come, let's flip the script together.
-                        <br/>
-                    </p>
-                </div>
             </div>
             <div className="projectsWrapper" id="projects">
-                <div className="projectsCard">
-                    <div className="projectsTitle">Projects</div>
-                    <div className="projectsBento">
-                        <div className="projectWrapper">
-                            <img className="projectImg" alt="ctn" src={ctnImg} />
-                            <p className="projectName">Clutch Time Network</p>
-                        </div>
-                        <div className="projectWrapper">
-                            <img className="projectImg" alt="lunasyz" src={lunasyzImg} />
-                            <p className="projectName">LunaSyz</p>
-                        </div>
-                        <div className="projectWrapper">
-                            <img className="projectImg" alt="fdweb" src={fdwebImg} />
-                            <p className="projectName">Fluent Design For Web</p>
-                        </div>
-                    </div>
-                </div>
             </div>
             <div className="socialsWrapper">
-                    <NavLink className="navLink" to="https://github.com/invrz" target="_blank">
-                        Star On GitHub
-                    </NavLink>
-                    <a className="navLink" onClick={() => {scrollToDiv('top')}}>
-                        Back To Top
-                    </a>
-                    <NavLink className="navLink" to="https://www.instagram.com/clutchtimenetwork/" target="_blank">
-                        Follow CTN
-                    </NavLink>
             </div>
         </Fragment>
     );
