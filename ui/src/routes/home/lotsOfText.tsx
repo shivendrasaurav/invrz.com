1a7cfd5effe3d011f51a679039ad6ced01256dad
Shivendra Saurav <shivendrasaurav@gmail.com>
2024-03-13 13:43:27 +0530

    added all content, animations tbd

--- ui/src/routes/home/lotsOfText.tsx
+++ ui/src/routes/home/lotsOfText.tsx
@@ -1,10 +1,7 @@
-import {Fragment, useEffect, useState} from "react";
+import {Fragment, useEffect} from "react";
 import "./home.styles.css";
 
 const Home = () => {
-
-    const [primeBg, setPrimeBg] = useState('');
-    const verbs = ["Innovation Revolutionized", "Invert Reality", "In the uniVerse"];
 
     const randomBgPalleteGenerator = () => {
 
@@ -16,10 +13,6 @@ const Home = () => {
         const textPrime = `rgb(${255 - redMain}, ${255 - greenMain}, ${255 - blueMain})`
         document.getElementById("hero").style.backgroundColor = bgPrime;
         document.getElementById("hero").style.color = textPrime;
-        document.getElementById("outro").style.backgroundColor = bgPrime;
-        document.getElementById("outro").style.color = textPrime;
-
-        setPrimeBg(bgPrime);
 
         
         const bgNav = `rgb(${redMain*0.4}, ${greenMain*0.4}, ${blueMain*0.4})`;
@@ -63,23 +56,25 @@ const Home = () => {
                 <a onClick={() => scrollToDiv("about")} className="navLink about">A</a>
                 <a onClick={() => scrollToDiv("services")} className="navLink services">A</a>
                 <a onClick={() => scrollToDiv("projects")} className="navLink projects">P</a>
-                <a onClick={() => scrollToDiv("outro")} className="navLink outro">P</a>
             </div>
             <div className="heroWrapper gridRow" id="hero">
                 <div className="logoWrapper">
-                    <p className="title colWd-9-ld colWd-12-sm">INVRZ</p>
+                    <p className="title">INVRZ</p>
                 </div>
                 <div className="textWrapper">
-                        <span>{verbs[0]}</span>
-                        <span style={{cursor: 'pointer'}} onClick={()=>{location.reload()}}>Color ?</span>
-                        <span>{dateToday}</span>
+                    <span>Innovation Revolutionzed</span>
+                    <span style={{cursor: 'pointer'}} onClick={()=>{location.reload()}}>Color ?</span>
+                    <span>{dateToday}</span>
                 </div>
             </div>
             <div className="aboutWrapper gridRow rowCenter" id="about">
-                <div className="colWd-10-ld colWd-12-sm">
-                    <h1 className="fontUnica" style={{fontSize: '5.6rem', margin: '2rem'}}>who we are</h1>
-                    <p className="fontArchivo" style={{fontSize: '1.8rem', lineHeight: 1.5, letterSpacing: '1.6px', margin: '2rem'}}>
-                        We are an indie app studio based in Bharat which specializes in crafting meaningful brands via digital media. We are passionate about creating unique experiences tailored to express their own unique stories.
+                <div className="colWd-5-ld colWd-12-sm">
+                    <h1 className="fontUnica" style={{fontSize: '8.1rem', margin: '2rem'}}>who we are</h1>
+                </div>
+                <div className="gutterWd-1 colWd-0-sm"></div>
+                <div className="colWd-5-ld colWd-12-sm">
+                    <p className="fontArchivo taJustify" style={{fontSize: '1.8rem', lineHeight: 1.5, letterSpacing: '1.6px', margin: '2rem'}}>
+                        invrz is an indie app studio based in Bharat which specializes in building responsive websites, progressive web apps, cross platform native apps and long form content. Our team has a unique knack for designing stuff that you can't help but fall in love with.
                         <br/><br/>
                     </p>
                 </div>
@@ -90,17 +85,15 @@ const Home = () => {
                     <div className="colWd-6-ld colWd-12-sm card">
                         <h1 className="fontUnica">RESILIENT</h1>
                         <p className="fontArchivo">
-                            Building scalable and robust PWAs
+                            We specialize in creating cutting-edge solutions that seemlessly blend the best of both worlds: The versatility of web applications and the robustness of platform-specific apps. Our forte lies in crafting resilient Progressive Web Apps which can work offline, update in background and allow faster response times.
                         </p>
-                        <div className="img" style={{backgroundColor: `${primeBg}`, backgroundImage: "url('./assets/pwa.avif')"}} />
                     </div>
                     <div className="gutterWd-1 colWd-0-sm"></div>
                     <div className="colWd-3-ld colWd-12-sm card">
                         <h1 className="fontUnica">VERSATILE</h1>
                         <p className="fontArchivo">
-                            Producing cross platform native apps
+                            We recognize the importance of reaching a wider audience without compromising on quality. And our ability to build versatile Cross-platform native apps allows us to achieve just that.
                         </p>
-                        <div className="img" style={{backgroundColor: `${primeBg}`, backgroundImage: "url('./assets/cpa.avif')"}} />
                     </div>
                 </div>
                 <br/>
@@ -108,17 +101,15 @@ const Home = () => {
                     <div className="colWd-3-ld colWd-12-sm card">
                         <h1 className="fontUnica">INNOVATIVE</h1>
                         <p className="fontArchivo">
-                            Unique experiences
+                            Your portfolio is more than just a website to show your projects. We understand that and design every portfolio to set a narrative, make it come to life with interactive experiences and tell a unique story.
                         </p>
-                        <div className="img" style={{backgroundColor: `${primeBg}`, backgroundImage: "url('./assets/port.avif')", backgroundSize: '260%'}} />
                     </div>
                     <div className="gutterWd-1 colWd-0-sm"></div>
                     <div className="colWd-6-ld colWd-12-sm card">
-                        <h1 className="fontUnica">DYNAMIC</h1>
+                        <h1 className="fontUnica">PROVEN</h1>
                         <p className="fontArchivo">
-                            Deliverting success
+                            Our commitment to resilience, versatility, and innovation doesn’t end with development; it’s vividly reflected in the projects we bring to life. From the sleek design to the intuitive interfaces, we work on projects which are a testament to our dedication to excellence and diversity of abilities.
                         </p>
-                        <div className="img" style={{backgroundColor: `${primeBg}`, backgroundImage: "url('./assets/dyn.avif')" }} />
                     </div>
                 </div>
                 
@@ -131,7 +122,7 @@ const Home = () => {
                     <div className="gutterWd-1"></div>
                     <div className="colWd-5-ld colWd-12-sm">
                         <ul className="projectsList" style={{margin: '2rem'}}>
-                            <li>
+                            <li onMouseOver={()=>{alert('add tooltip here')}}>
                                 <h2 className="itemName fontArchivo">Clutch Time Network</h2>
                                 <p className="timeFrame fontArchivo">Ongoing</p>
                             </li>
@@ -159,26 +150,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className="outroWrapper" id="outro">
-                <div className="gridRow rowCenter">
-                    <div className="colWd-10-ld colWd-12-sm">
-                        <br/><br/>
-                        <br/><br/>
-                        <h1 className="fontUnica">we transform ideas into apps</h1>
-                        <h1 className="fontUnica">we build platform agnostic apps</h1>
-                        <h1 className="fontUnica">we flip reality</h1>
-                        <br/><br/>
-                        <a target="_blank" className="fontArchivo" href="https://github.com/invrz">Star on GitHub</a>
-                        <a target="_blank" className="fontArchivo" href="https://www.instagram.com/clutchtimenetwork/">Follow CTN</a>
-                        <a className="fontArchivo" onClick={() => scrollToDiv("hero")} >Back to top</a>
-                        <br/><br/>
-                        <br/><br/>
-                        <label>images cc unsplash.com</label><br/>
-                        <label>website design by Shivendra Saurav</label><br/>
-                        <label>&copy; invrz.com 2024</label>
-                        <br/><br/>
-                    </div>
-                </div>
+            <div className="outroWrapper">
             </div>
             <div className="socialsWrapper">
             </div>
